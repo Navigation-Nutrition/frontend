fc86d39aceb8edeabf14bceb347997798687a9f0
Celybot <youancece@gmail.com>
2023-02-27 19:06:35 -0500

    cleared recipe page and quiz page

--- navnut/src/Components/MealItem.js
+++ navnut/src/Components/MealItem.js
@@ -8,11 +8,11 @@ const Mealitem=(getMeal)=>{
                         <img src={getMeal.data.strMealThumb} alt="meal"/>
                         <div className ="info">
                         <h2>{getMeal.data.strMeal}</h2>
-                        <p>{getMeal.data.strArea} food</p>
+                        <p className ="foodtype"> {getMeal.data.strArea} food</p>
                         </div>
                         <div className ="recipe">
                             <h2>Recipe</h2>
-                            <p>{getMeal.data.strInstructions}</p>
+                            <p> {getMeal.data.strInstructions}</p>
                             <img src={getMeal.data.strMealThumb}/>
                             <a href={getMeal.data.strSource}>Get Full Recipe</a>
                         </div>
